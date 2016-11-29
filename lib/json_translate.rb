2bff2a5ede0da1d6e0cc2432bbe9cbc0cc20e354
Cédric Fabianski <cfabianski@me.com>
2016-11-29 22:47:46 +0100

    Move instance_methods into its own file

--- lib/json_translate.rb
+++ lib/json_translate.rb
@@ -1,4 +1,5 @@
-require 'active_record'
-require 'json_translate/translates'
+require "active_record"
+require "json_translate/translates"
+require "json_translate/translates/instance_methods"
 
 ActiveRecord::Base.extend(JSONTranslate::Translates)
