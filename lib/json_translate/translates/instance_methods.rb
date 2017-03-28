4c44e23eb3eb50226e591129cadb427a24353470
Georgi Kirichkov <kirichkov@users.noreply.github.com>
2017-03-28 16:02:27 +0300

    Fixes parsing of attribute names containing digits

--- lib/json_translate/translates/instance_methods.rb
+++ lib/json_translate/translates/instance_methods.rb
@@ -69,7 +69,7 @@ module JSONTranslate
       # Returns the attribute name Symbol, locale Symbol, and a Boolean
       # indicating whether or not the caller is attempting to assign a value.
       def parse_translated_attribute_accessor(method_name)
-        return unless /\A(?<attribute>[a-z_]+)_(?<locale>[a-z]{2})(?<assignment>=?)\z/ =~ method_name
+        return unless /\A(?<attribute>[a-z0-9_]+)_(?<locale>[a-z]{2})(?<assignment>=?)\z/ =~ method_name
 
         translated_attr_name = attribute.to_sym
         return unless translated_attribute_names.include?(translated_attr_name)
