a29770c121a4dac4ad9eaf1a3687212eb4ed01c9
Kristaps Kulikovskis <kristaps.kulikovskis@makit.lv>
2017-07-23 20:30:55 +0300

    test interpolation missing

--- test/translates_test.rb
+++ test/translates_test.rb
@@ -142,12 +142,17 @@ class TranslatesTest < JSONTranslate::Test
     end
   end
 
-  def test_with_translation_attributes
+  def test_with_interpolation_arguments
     p = Post.create!(:title_translations => { "en" => "Alice in %{where}" })
     I18n.with_locale(:en) do
       assert_equal p.title(where: "Wonderland"), "Alice in Wonderland"
     end
     assert_equal p.title_en(where: "Wonderland"), "Alice in Wonderland"
+  end
+
+  def test_for_missing_interpolation_arguments
+    p = Post.create!(:title_translations => { "en" => "Alice in %{where}" })
+    assert_equal p.title_en, "Alice in %{where}"
   end
 
   def test_class_method_translates?
